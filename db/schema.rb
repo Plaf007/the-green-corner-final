05dc14b512554e5bfcb827bdc1905b164b03bef3
Marco Soria <marco.soria@pucp.edu.pe>
2023-12-17 20:54:22 -0500

    gem pgsearch y cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,83 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_18_004622) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_18_013610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "details"
+    t.float "longitude"
+    t.float "latitude"
+    t.string "addressable_type", null: false
+    t.bigint "addressable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "purchase_date"
+    t.decimal "total_due"
+    t.integer "status"
+    t.decimal "virtual_cash"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "details"
+    t.integer "category"
+    t.integer "quantity"
+    t.decimal "price"
+    t.decimal "virtual_cash"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "recycle_points", force: :cascade do |t|
+    t.integer "category"
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.string "reviewable_type", null: false
+    t.bigint "reviewable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable"
+  end
+
+  create_table "selected_products", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "price"
+    t.decimal "virtual_cash"
+    t.bigint "product_id", null: false
+    t.string "selected_productable_type", null: false
+    t.bigint "selected_productable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_selected_products_on_product_id"
+    t.index ["selected_productable_type", "selected_productable_id"], name: "index_selected_products_on_selected_productable"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +100,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_18_004622) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
+  add_foreign_key "orders", "users"
+  add_foreign_key "products", "users"
+  add_foreign_key "selected_products", "products"
 end

e86dd20c137cfe879a5439921ff1905d887c483a
Leonardo Diaz <leo.diaz.trejo@gmail.com>
2024-01-20 08:07:04 -0600

    debugged carrito

--- db/migrate/20231219235518_add_information_to_users.rb
+++ db/migrate/20231219235518_add_information_to_users.rb
@@ -2,6 +2,6 @@ class AddInformationToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :total_virtual_cash, :decimal, precision: 10, scale: 2
+    add_column :users, :total_virtual_cash, :decimal, precision: 10, scale: 2, default: 0.0
   end
 end
